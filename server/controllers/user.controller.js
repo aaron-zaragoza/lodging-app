06beb8881d9b232d0f11fe47910461a41a115833
aaron-zaragoza <76231856+aaron-zaragoza@users.noreply.github.com>
2021-05-21 19:03:14 -0700

    Created user controller

---
+++ server/controllers/user.controller.js
@@ -0,0 +1,11 @@
+const Entry = require("../models/entry.model");
+
+module.exports = {
+    register: (req, res) => {
+        User.create(req.body)
+        .then(user => {
+            res.json({ msg: "success!", user: user });
+        })
+        .catch(err => res.json(err));
+    }
+}
